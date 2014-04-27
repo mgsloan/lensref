759c46572c5dd9c8249541911b47391968a16ca2
divip <divipp@gmail.com>
2014-04-27 23:09:22 +0200

    reorder modules

---
+++ LensRef.hs
@@ -0,0 +1,356 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Data.LensRef where
+
+import Control.Monad (liftM)
+import Control.Lens (Lens', lens, set, (^.))
+
+--------------------------------
+
+-- | @m@ has a submonad @(RefState m)@ which is isomorphic to 'Reader'.
+class (Monad m, Monad (RefState m)) => MonadRefReader m where
+
+    {- | Law: @(RefState m)@  ===  @('Reader' x)@ for some @x@.
+
+    Alternative laws which ensures this isomorphism (@r :: (RefState m a)@ is arbitrary):
+
+     *  @(r >> return ())@ === @return ()@
+
+     *  @liftM2 (,) r r@ === @liftM (\a -> (a, a)) r@
+
+    See also <http://stackoverflow.com/questions/16123588/what-is-this-special-functor-structure-called>
+    -}
+    data RefState m a :: *
+
+    -- | @m@ is a submonad of @(RefState m)@
+    liftRefStateReader :: m a -> RefState m a
+
+
+
+{- |
+A reference @(r a)@ is isomorphic to @('Lens' s a)@ for some fixed state @s@.
+
+@r@  ===  @Lens s@
+-}
+class (MonadRefReader (RefReader r)) => Reference r where
+
+    {- | @Refmonad r@  ===  @State s@
+
+    Property derived from the 'MonadRefReader' instance:
+
+    @RefReader r@ = @RefState (Refmonad r)@  ===  @Reader s@
+    -}
+    type RefReader r :: * -> *
+
+    {- | @readRef@ === @reader . getL@
+
+    Properties derived from the 'MonadRefReader' instance:
+
+    @(readRef r >> return ())@ === @return ()@
+    -}
+    readRef  :: MRef r a -> RefReader r a
+
+    {- | @writeRef r@ === @modify . set r@
+
+    Properties derived from the set-get, get-set and set-set laws for lenses:
+
+     *  @(readRef r >>= writeRef r)@ === @return ()@
+
+     *  @(writeRef r a >> readRef r)@ === @return a@
+
+     *  @(writeRef r a >> writeRef r a')@ === @writeRef r a'@
+    -}
+    writeRef :: MRef r a -> a -> RefState (RefReader r) ()
+
+    {- | Apply a lens on a reference.
+
+    @lensMap@ === @(.)@
+    -}
+    lensMap :: Lens' a b -> MRef r a -> MRef r b
+
+    -- | @unitRef@ === @lens (const ()) (const id)@
+    unitRef :: MRef r ()
+
+-- | Reference wrapped into a RefReader monad
+type MRef r a = RefReader r (r a)
+
+infixr 8 `lensMap`
+
+
+{- | Monad for reference creation. Reference creation is not a method
+of the 'Reference' type class to make possible to
+create the same type of references in multiple monads.
+
+@(Extref m) === (StateT s m)@, where 's' is an extendible state.
+
+For basic usage examples, look into the source of @Data.LensRef.Pure.Test@.
+-}
+class (Monad m, Reference (RefCore m)) => ExtRef m where
+
+    type RefCore m :: * -> *
+
+    {- | @ReadRef@ lifted to the reference creation class.
+
+    Note that we do not lift @WriteRef@ to the reference creation class, which a crucial restriction
+    in the LGtk interface; this is a feature.
+    -}
+    liftReadRef :: ExtRef m => ReadRef m a -> m a
+
+    {- | Reference creation by extending the state of an existing reference.
+
+    Suppose that @r@ is a reference and @k@ is a lens.
+
+    Law 1: @extRef@ applies @k@ on @r@ backwards, i.e. 
+    the result of @(extRef r k a0)@ should behaves exactly as @(lensMap k r)@.
+
+     *  @(liftM (k .) $ extRef r k a0)@ === @return r@
+
+    Law 2: @extRef@ does not change the value of @r@:
+
+     *  @(extRef r k a0 >> readRef r)@ === @(readRef r)@
+
+    Law 3: Proper initialization of newly defined reference with @a0@:
+
+     *  @(extRef r k a0 >>= readRef)@ === @(readRef r >>= set k a0)@
+    -}
+    extRef :: Ref m b -> Lens' a b -> a -> m (Ref m a)
+
+    {- | @newRef@ extends the state @s@ in an independent way.
+
+    @newRef@ === @extRef unitRef (lens (const ()) (const id))@
+    -}
+    newRef :: a -> m (Ref m a)
+    newRef = extRef unitRef $ lens (const ()) (flip $ const id)
+
+
+    {- | Lazy monadic evaluation.
+    In case of @y <- memoRead x@, invoking @y@ will invoke @x@ at most once.
+
+    Laws:
+
+     *  @(memoRead x >> return ())@ === @return ()@
+
+     *  @(memoRead x >>= id)@ === @x@
+
+     *  @(memoRead x >>= \y -> liftM2 (,) y y)@ === @liftM (\a -> (a, a)) y@
+
+     *  @(memoRead x >>= \y -> liftM3 (,) y y y)@ === @liftM (\a -> (a, a, a)) y@
+
+     *  ...
+    -}
+    memoRead :: ExtRef m => m a -> m (m a)
+
+    memoWrite :: (ExtRef m, Eq b) => (b -> m a) -> m (b -> m a)
+
+    future :: (ReadRef m a -> m a) -> m a
+
+
+type Ref m a = ReadRef m (RefCore m a)
+
+type ReadRef m = RefReader (RefCore m)
+
+type WriteRef m = RefState (ReadRef m)
+
+
+-- | Monad for dynamic actions
+class (ExtRef m, ExtRef (Modifier m), RefCore (Modifier m) ~ RefCore m) => EffRef m where
+
+    type EffectM m :: * -> *
+
+    data Modifier m a :: *
+
+    liftEffectM :: EffectM m a -> m a
+
+    liftModifier :: m a -> Modifier m a
+
+    liftWriteRef' :: WriteRef m a -> Modifier m a
+
+    {- |
+    Let @r@ be an effectless action (@ReadRef@ guarantees this).
+
+    @(onChange init r fmm)@ has the following effect:
+
+    Whenever the value of @r@ changes (with respect to the given equality),
+    @fmm@ is called with the new value @a@.
+    The value of the @(fmm a)@ action is memoized,
+    but the memoized value is run again and again.
+
+    The boolean parameter @init@ tells whether the action should
+    be run in the beginning or not.
+
+    For example, let @(k :: a -> m b)@ and @(h :: b -> m ())@,
+    and suppose that @r@ will have values @a1@, @a2@, @a3@ = @a1@, @a4@ = @a2@.
+
+    @onChange True r $ \\a -> k a >>= return . h@
+
+    has the effect
+
+    @k a1 >>= \\b1 -> h b1 >> k a2 >>= \\b2 -> h b2 >> h b1 >> h b2@
+
+    and
+
+    @onChange False r $ \\a -> k a >>= return . h@
+
+    has the effect
+
+    @k a2 >>= \\b2 -> h b2 >> k a1 >>= \\b1 -> h b1 >> h b2@
+    -}
+    onChange_
+        :: Eq b
+        => ReadRef m b
+        -> b -> (b -> c)
+        -> (b -> b -> c -> m (c -> m c))
+        -> m (ReadRef m c)
+
+    toReceive :: Functor f => f (Modifier m ()) -> (Command -> EffectM m ()) -> m (f (EffectM m ()))
+
+data Command = Kill | Block | Unblock deriving (Eq, Ord, Show)
+
+
+
+
+
+-------------- derived constructs
+
+
+{- | @readRef@ lifted to the reference creation class.
+
+@readRef'@ === @liftReadRef . readRef@
+-}
+readRef' :: ExtRef m => Ref m a -> m a
+readRef' = liftReadRef . readRef
+
+
+
+{- | References with inherent equivalence.
+
+-}
+class Reference r => EqReference r where
+    valueIsChanging :: MRef r a -> RefReader r (a -> Bool)
+
+{- | @hasEffect r f@ returns @False@ iff @(modRef m f)@ === @(return ())@.
+
+@hasEffect@ is correct only if @eqRef@ is applied on a pure reference (a reference which is a pure lens on the hidden state).
+
+@hasEffect@ makes defining auto-sensitive buttons easier, for example.
+-}
+hasEffect
+    :: EqReference r
+    => MRef r a
+    -> (a -> a)
+    -> RefReader r Bool
+hasEffect r f = do
+    a <- readRef r
+    ch <- valueIsChanging r
+    return $ ch $ f a
+
+
+data EqRefCore r a = EqRefCore (r a) (a -> Bool{-changed-})
+
+{- | References with inherent equivalence.
+
+@EqRef r a@ === @RefReader r (exist b . Eq b => (Lens' b a, r b))@
+
+As a reference, @(m :: EqRef r a)@ behaves as
+
+@join $ liftM (uncurry lensMap) m@
+-}
+type EqRef r a = RefReader r (EqRefCore r a)
+
+{- | @EqRef@ construction.
+-}
+eqRef :: (Reference r, Eq a) => MRef r a -> EqRef r a
+eqRef r = do
+    a <- readRef r
+    r_ <- r
+    return $ EqRefCore r_ $ (/= a)
+
+newEqRef :: (ExtRef m, Eq a) => a -> m (EqRef (RefCore m) a) 
+newEqRef = liftM eqRef . newRef
+
+{- | An @EqRef@ is a normal reference if we forget about the equality.
+
+@toRef m@ === @join $ liftM (uncurry lensMap) m@
+-}
+toRef :: Reference r => EqRef r a -> MRef r a
+toRef m = m >>= \(EqRefCore r _) -> return r
+
+instance Reference r => EqReference (EqRefCore r) where
+    valueIsChanging m = do
+        EqRefCore _r k <- m
+        return k
+
+instance Reference r => Reference (EqRefCore r) where
+
+    type (RefReader (EqRefCore r)) = RefReader r
+
+    readRef = readRef . toRef
+
+    writeRef = writeRef . toRef
+
+    lensMap l m = do
+        a <- readRef m
+        EqRefCore r k <- m
+        lr <- lensMap l $ return r
+        return $ EqRefCore lr $ \b -> k $ set l b a
+
+    unitRef = eqRef unitRef
+
+
+data CorrRefCore r a = CorrRefCore (r a) (a -> Maybe a{-corrected-})
+
+type CorrRef r a = RefReader r (CorrRefCore r a)
+
+instance Reference r => Reference (CorrRefCore r) where
+
+    type (RefReader (CorrRefCore r)) = RefReader r
+
+    readRef = readRef . fromCorrRef
+
+    writeRef = writeRef . fromCorrRef
+
+    lensMap l m = do
+        a <- readRef m
+        CorrRefCore r k <- m
+        lr <- lensMap l $ return r
+        return $ CorrRefCore lr $ \b -> fmap (^. l) $ k $ set l b a
+
+    unitRef = corrRef (const Nothing) unitRef
+
+fromCorrRef :: Reference r => CorrRef r a -> MRef r a
+fromCorrRef m = m >>= \(CorrRefCore r _) -> return r
+
+corrRef :: Reference r => (a -> Maybe a) -> MRef r a -> CorrRef r a
+corrRef f r = do
+    r_ <- r
+    return $ CorrRefCore r_ f
+
+correction :: Reference r => CorrRef r a -> RefReader r (a -> Maybe a)
+correction r = do
+    CorrRefCore _ f <- r
+    return f
+
+----------------
+
+rEffect  :: (EffRef m, Eq a) => ReadRef m a -> (a -> EffectM m b) -> m (ReadRef m b)
+rEffect r f = onChange r $ return . liftEffectM . f
+
+onChange :: (EffRef m, Eq a) => ReadRef m a -> (a -> m (m b)) -> m (ReadRef m b)
+onChange r f = onChange_ r undefined undefined $ \b _ _ -> liftM (\x _ -> x) $ f b
+
+writeRef' :: (EffRef m, Reference r, RefReader r ~ RefReader (RefCore m)) => MRef r a -> a -> Modifier m ()
+writeRef' r a = liftWriteRef' $ writeRef r a
+
+-- | @modRef r f@ === @liftRefStateReader (readRef r) >>= writeRef r . f@
+--modRef :: Reference r => MRef r a -> (a -> a) -> RefStateReader (RefReader r) ()
+r `modRef'` f = liftRefStateReader' (readRef r) >>= writeRef' r . f
+
+liftRefStateReader' :: EffRef m => ReadRef m a -> Modifier m a
+liftRefStateReader' r = liftWriteRef' $ liftRefStateReader r
+
+action' m = liftModifier $ liftEffectM m
+
+
+
+
---
+++ LensRef/Test.hs
@@ -0,0 +1,363 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+-- | Tests for the @ExtRef@ interface.
+module Data.LensRef.Test
+    ( -- * Tests for the interface
+      mkTests
+    -- * Tests for implementations
+    , testExtPure
+--    , testExtIORef
+    ) where
+
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Concurrent
+import Control.Category
+--import Control.Arrow ((***))
+import Data.Maybe
+import Prelude hiding ((.), id)
+import System.IO
+
+import Control.Lens
+import Data.LensRef
+--import Data.LensRef.Pure (ExtRefWrite (..))
+import qualified Data.LensRef.Pure as Pure
+
+import System.IO.Unsafe
+
+-----------------------------------------------------------------
+
+
+{-
+
+-- | This instance is used in the implementation, end users do not need it.
+instance (ExtRef m, Monoid w) => ExtRef (WriterT w m) where
+
+    type RefCore (WriterT w m) = RefCore m
+
+    liftReadRef = lift . liftReadRef
+
+    extRef x y a = lift $ extRef x y a
+
+instance (ExtRefWrite m, Monoid w) => ExtRefWrite (WriterT w m) where
+
+    liftWriteRef = lift . liftWriteRef
+-}
+
+-- | Consistency tests for the pure implementation of @Ext@, should give an empty list of errors.
+{-
+testExtPure :: [String]
+testExtPure = mkTests $ \t -> let
+    (((), m), w) = runWriter $ Pure.runPure newChan t
+    in w -- ++ execWriter m
+  where
+    newChan = return (fail "x", \_ -> return (error "1"))
+-}
+
+instance MonadWriter [String] IO where
+    tell = putStrLn . show
+
+testExtPure = mkTests $ \t -> unsafePerformIO $ do
+    hSetBuffering stdout LineBuffering
+    ((), m) <- Pure.runPure newChan' t
+
+    forkIO m
+    return ["end"]
+   where
+    newChan' :: IO (IO a, a -> IO ())
+    newChan' = do
+        ch <- newChan
+        return (readChan ch, \x -> writeChan ch x)
+
+
+-- | Check an equality.
+(==?) :: (Eq a, Show a, MonadWriter [String] (EffectM m), EffRef m) => a -> a -> m ()
+rv ==? v = liftEffectM $ when (rv /= v) $ tell . return $ "runTest failed: " ++ show rv ++ " /= " ++ show v
+
+-- | Check the current value of a given reference.
+(==>) :: (Eq a, Show a, MonadWriter [String] (EffectM m), EffRef m) => Ref m a -> a -> m ()
+r ==> v = readRef' r >>= (==? v)
+
+infix 0 ==>, ==?
+
+maybeLens :: Lens' (Bool, a) (Maybe a)
+maybeLens = lens (\(b,a) -> if b then Just a else Nothing)
+              (\(_,a) x -> maybe (False, a) (\a' -> (True, a')) x)
+
+{- | 
+@mkTests@ generates a list of error messages which should be emtpy.
+
+Look inside the sources for the tests.
+-}
+mkTests :: ((forall m . (MonadWriter [String] (EffectM m), EffRef m) => m ()) -> [String]) -> [String]
+mkTests runTest
+      = newRefTest
+     ++ writeRefTest
+{-
+     ++ writeRefsTest
+     ++ extRefTest
+     ++ joinTest
+     ++ joinTest2
+     ++ chainTest0
+     ++ forkTest
+     ++ forkTest2
+     ++ chainTest
+     ++ undoTest
+     ++ undoTest2
+-}
+--     ++ undoTest3
+  where
+
+    newRefTest = runTest $ do
+        r <- newRef (3 :: Int)
+        r ==> 3
+
+    writeRefTest = runTest $ do
+        r <- newRef (3 :: Int)
+        k <- newRef (3 :: Int)
+        sr <- toReceive (writeRef' r) (const $ return ())
+        sk <- toReceive (writeRef' k) (const $ return ())
+
+        onChange (readRef r) $ \x -> do
+            when (x == 3) $ do
+                onChange (readRef k) $ \x -> do
+                    liftEffectM $ tell ["k1: " ++ show x]
+                    return $ return ()
+                return ()
+            return $ do
+                liftEffectM $ tell ["r: " ++ show x]
+                when (x == 4) $ do
+                    onChange (readRef k) $ \x -> return $ do
+                        liftEffectM $ tell ["k2: " ++ show x]
+                    return ()
+                r ==> x
+        r ==> 3
+        liftEffectM $ sr 5
+        liftEffectM $ sk 6
+        liftEffectM $ sr 4
+        liftEffectM $ sk 7
+        liftEffectM $ sr 3
+        liftEffectM $ sk 8
+        liftEffectM $ sr 5
+        liftEffectM $ sk 9
+        liftEffectM $ sr 4
+        liftEffectM $ sk 10
+        liftEffectM $ sr 3
+        liftEffectM $ sk 11
+        r ==> 3
+        return ()
+
+{-
+    writeRefsTest = runTest $ do
+        r1 <- newRef (3 :: Int)
+        r2 <- newRef (13 :: Int)
+        r1 ==> 3
+        r2 ==> 13
+        writeRef' r1 4
+        r1 ==> 4
+        r2 ==> 13
+        writeRef' r2 0
+        r1 ==> 4
+        r2 ==> 0
+
+    extRefTest = runTest $ do
+        r <- newRef $ Just (3 :: Int)
+        q <- extRef r maybeLens (False, 0)
+        let q1 = _1 `lensMap` q
+            q2 = _2 `lensMap` q
+        r ==> Just 3
+        q ==> (True, 3)
+        writeRef' r Nothing
+        r ==> Nothing
+        q ==> (False, 3)
+        q1 ==> False
+        writeRef' q1 True
+        r ==> Just 3
+        writeRef' q2 1
+        r ==> Just 1
+
+    joinTest = runTest $ do
+        r2 <- newRef (5 :: Int)
+        r1 <- newRef 3
+        rr <- newRef r1
+        r1 ==> 3
+        let r = join' rr
+        r ==> 3
+        writeRef' r1 4
+        r ==> 4
+        writeRef' rr r2
+        r ==> 5
+        writeRef' r1 4
+        r ==> 5
+        writeRef' r2 14
+        r ==> 14
+
+    joinTest2 = runTest $ do
+        r1 <- newRef (3 :: Int)
+        rr <- newRef r1
+        r2 <- newRef 5
+        writeRef' rr r2
+        join' rr ==> 5
+
+    chainTest0 = runTest $ do
+        r <- newRef (1 :: Int)
+        q <- extRef r id 0
+        s <- extRef q id 0
+        r ==> 1
+        q ==> 1
+        s ==> 1
+        writeRef' r 2
+        r ==> 2
+        q ==> 2
+        s ==> 2
+        writeRef' q 3
+        r ==> 3
+        q ==> 3
+        s ==> 3
+        writeRef' s 4
+        r ==> 4
+        q ==> 4
+        s ==> 4
+
+    forkTest = runTest $ do
+        r <- newRef (1 :: Int)
+        q <- extRef r id 0
+        s <- extRef r id 0
+        r ==> 1
+        q ==> 1
+        s ==> 1
+        writeRef' r 2
+        r ==> 2
+        q ==> 2
+        s ==> 2
+        writeRef' q 3
+        r ==> 3
+        q ==> 3
+        s ==> 3
+        writeRef' s 4
+        r ==> 4
+        q ==> 4
+        s ==> 4
+
+    forkTest2 = runTest $ do
+        r <- newRef $ Just (1 :: Int)
+        q <- extRef r maybeLens (False, 0)
+        s <- extRef r maybeLens (False, 0)
+        r ==> Just 1
+        q ==> (True, 1)
+        s ==> (True, 1)
+        writeRef' r $ Just 2
+        r ==> Just 2
+        q ==> (True, 2)
+        s ==> (True, 2)
+        writeRef' r Nothing
+        r ==> Nothing
+        q ==> (False, 2)
+        s ==> (False, 2)
+        writeRef' (_1 `lensMap` q) True
+        r ==> Just 2
+        q ==> (True, 2)
+        s ==> (True, 2)
+        writeRef' (_2 `lensMap` q) 3
+        r ==> Just 3
+        q ==> (True, 3)
+        s ==> (True, 3)
+        writeRef' (_1 `lensMap` q) False
+        r ==> Nothing
+        q ==> (False, 3)
+        s ==> (False, 3)
+        writeRef' (_2 `lensMap` q) 4
+        r ==> Nothing
+        q ==> (False, 4)
+        s ==> (False, 3)
+        writeRef' (_1 `lensMap` q) True
+        r ==> Just 4
+        q ==> (True, 4)
+        s ==> (True, 4)
+        writeRef' q (False, 5)
+        r ==> Nothing
+        q ==> (False, 5)
+        s ==> (False, 4)
+        writeRef' (_1 `lensMap` s) True
+        r ==> Just 4
+        q ==> (True, 4)
+        s ==> (True, 4)
+
+    chainTest = runTest $ do
+        r <- newRef $ Just $ Just (3 :: Int)
+        q <- extRef r maybeLens (False, Nothing)
+        s <- extRef (_2 `lensMap` q) maybeLens (False, 0)
+        r ==> Just (Just 3)
+        q ==> (True, Just 3)
+        s ==> (True, 3)
+        writeRef' (_1 `lensMap` s) False
+        r ==> Just Nothing
+        q ==> (True, Nothing)
+        s ==> (False, 3)
+        writeRef' (_1 `lensMap` q) False
+        r ==> Nothing
+        q ==> (False, Nothing)
+        s ==> (False, 3)
+        writeRef' (_1 `lensMap` s) True
+        r ==> Nothing
+        q ==> (False, Just 3)
+        s ==> (True, 3)
+        writeRef' (_1 `lensMap` q) True
+        r ==> Just (Just 3)
+        q ==> (True, Just 3)
+        s ==> (True, 3)
+
+    undoTest = runTest $ do
+        r <- newRef (3 :: Int)
+        q <- extRef r (lens head $ flip (:)) []
+        writeRef' r 4
+        q ==> [4, 3]
+
+    undoTest2 = runTest $ do
+        r <- newRef (3 :: Int)
+        q <- extRef r (lens head $ flip (:)) []
+        q ==> [3]
+{-
+    undoTest3 = runTest $ do
+        r <- newRef 3
+        (undo, redo) <- liftM (liftRefStateReader *** liftRefStateReader) $ undoTr (==) r
+        r ==> 3
+        redo === False
+        undo === False
+        writeRef' r 4
+        r ==> 4
+        redo === False
+        undo === True
+        writeRef' r 5
+        r ==> 5
+        redo === False
+        undo === True
+        push undo
+        r ==> 4
+        redo === True
+        undo === True
+        push undo
+        r ==> 3
+        redo === True
+        undo === False
+        push redo
+        r ==> 4
+        redo === True
+        undo === True
+        writeRef' r 6
+        r ==> 6
+        redo === False
+        undo === True
+      where
+        push m = liftWriteRef m >>= \x -> maybe (return ()) liftWriteRef x
+        m === t = liftWriteRef m >>= \x -> isJust x ==? t
+-}
+    join' r = join $ readRef r
+
+    writeRef' r a = liftWriteRef $ writeRef r a
+-}
