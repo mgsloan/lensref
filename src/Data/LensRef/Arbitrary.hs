1f5d7cf860c08298103d9130488b727d6c0f39f4
divip <divipp@gmail.com>
2014-05-28 10:45:48 +0200

    begin to implement Arbitrary instances

---
+++ src/Data/LensRef/Arbitrary.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGe TypeSynonymInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+module Data.LensRef.Arbitrary where
+
+import Control.Applicative
+import Control.Monad.Identity
+import Control.Monad.Operational
+import Test.QuickCheck
+
+import Data.LensRef.Class
+import Data.LensRef.TestEnv
+import qualified Data.LensRef.Pure as Pure
+import qualified Data.LensRef.Fast as Fast
+
+--------------------------------
+
+data RegisterI a where
+    OnChange :: Eq a => ARefReader a -> (a -> ARegister b) -> RegisterI (ARefReader b)
+
+type ARegister = Program RegisterI
+
+data RefReaderI a where
+    X :: RefReaderI ()
+
+type ARefReader = Program RefReaderI
+
+---------------------------------
+
+instance Show (ARegister a) where
+
+---------------------------------
+
+instance Arbitrary (ARefReader Bool) where
+
+instance Arbitrary (ARegister ()) where
+    arbitrary = oneof
+        [ fmap return arbitrary
+        ]
+
+instance Arbitrary (ARegister Bool) where
+    arbitrary = oneof
+        [ fmap return arbitrary
+        ]
+
+instance Arbitrary (ARegister (ARefReader Bool)) where
+    arbitrary = oneof
+        [ fmap return arbitrary
+        , liftA2 (\r f -> singleton $ OnChange (r :: ARefReader Bool) f) arbitrary arbitrary
+        ]
+
+toPure :: ARegister a -> Pure.Register Identity a
+toPure = undefined
+
+toFast :: ARegister a -> Fast.Register Identity a
+toFast = undefined
+
+similar :: Pure.Register Identity a -> Fast.Register Identity a -> Bool
+similar = undefined
+
+prop :: ARegister () -> Bool
+prop m = similar (toPure m) (toFast m)
+
+test = quickCheck prop
+
+
